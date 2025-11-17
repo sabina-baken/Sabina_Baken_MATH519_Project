c45859f953bea56a9e4494921e9dfd6611719511
Сабина <@10.101.15.243>
2025-11-17 16:13:45 +0500

    Looping for Jacobian at nontrivial + logistic model

--- PROJECT.m
+++ PROJECT.m
@@ -2,15 +2,16 @@
 clear; close all; clc;
 %%Ordinary model and it's stability
 syms x y real
-a_val=1;
-b_val=1;
-c_val=1;
-d_val=1;
+a_val=5;
+b_val=2;
+c_val=3;
+d_val=5;
 a = a_val;
 b = b_val;
 c = c_val;
 d = d_val;
-prey = a*x-b*x*y;
+k_prey = 10;
+prey = a*x*(1-x/k_prey)-b*x*y;
 predator = c*x*y-d*y;
 M = [prey;predator];
 J = jacobian(M, [x,y]);
@@ -19,3 +20,13 @@ disp(J);
 [x_eq,y_eq] = solve([prey==0, predator==0], [x,y]);
 disp('Equilibrium points are:');
 disp([x_eq,y_eq]);
+J_origin = subs(J, [x,y],[x_eq(1),y_eq(1)]);
+disp('Jacobian matrix at origin is:');
+disp(J_origin);
+
+num_eq = length(x_eq);
+for k = 2:num_eq
+J_nontr = subs(J, [x,y],[x_eq(k),y_eq(k)]);
+disp('Jacobian matrix at nontrivial equilibrium');
+disp(J_nontr);
+end
